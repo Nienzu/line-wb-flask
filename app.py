9dba91a50575146a77558c33915c4c0602db03ff
nienzu <ibqqz0602@gmail.com>
2016-12-25 00:00:41 +0800

    Use assert

--- app.py
+++ app.py
@@ -36,12 +36,8 @@ app = Flask(__name__)
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+assert channel_secret is not None,'Specify LINE_CHANNEL_SECRET as environment variable.'
+assert channel_access_token is not None,'Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.'
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
